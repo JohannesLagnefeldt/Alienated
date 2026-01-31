f37e412927cc3e5d8dc98a2b9196a2beefd55b72
JohannesLagnefeldt <johannes.lagnefeldt@gmail.com>
2026-01-31 15:32:41 +0100

    Update mask_resource.gd

--- scripts/mask_resource.gd
+++ scripts/mask_resource.gd
@@ -2,14 +2,22 @@ extends Resource
 class_name MaskResource
 
 enum SHAPE {SQUARE, ROUND, TRIANGLE}
-enum HORNS {STRAIGHT, BENT, SQIGGLY}
-enum DAMAGE {NONE, TOP, RIGHT, BOTTOM, LEFT}
+enum HORNS {STRAIGHT, BENT}
+enum DAMAGE {TOP, RIGHT, LEFT}
 
 @export var shape : SHAPE
-@export var horns : HORNS
-@export var damage : DAMAGE
+@export var horns_shape : HORNS
+@export var nbr_horns : int
+@export var chip_position : DAMAGE
+@export var nbr_chips : int
 @export var mask_texture : Texture2D
 
 func is_equall(other : MaskResource) -> bool:
-	return other.damage == self.damage and other.horns == self.horns and other.shape == self.shape
+	# not tested yet. remove comment when tested
+	var is_correct = other.shape == shape
+	is_correct = is_correct and other.horns_shape == horns_shape
+	is_correct = is_correct and other.nbr_horns == nbr_horns
+	is_correct = is_correct and other.chip_position == chip_position
+	is_correct = is_correct and other.nbr_chips == nbr_chips
+	return is_correct
 	
