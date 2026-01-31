127664e5042b87be2dd01abebbce467a94ad0e5e
SsbThatsMe <samuel.broms@gmail.com>
2026-01-31 15:06:21 +0100

    Added music and sfx

--- scripts/mask_container.gd
+++ scripts/mask_container.gd
@@ -3,16 +3,19 @@ extends HBoxContainer
 @export
 var input_masks = []
 
+@onready var audio_toggle_mask_2: AudioStreamPlayer2D = $AudioToggleMask2
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	for n in self.get_children():
-		remove_child(n)
+	#for n in self.get_children():
+	#	remove_child(n)
 	for i in Master.masks_in_guess:
 		var c = preload("uid://b3xqkba08wweq").instantiate()
 		add_child(c)
 		input_masks.append(c)
 	pass # Replace with function body.
-	
+
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventKey and event.is_pressed():
@@ -21,3 +24,4 @@ func _input(event: InputEvent) -> void:
 			for m in input_masks:
 				guess.append(m.index)
 			print(Master.try_solve(guess))
+			audio_toggle_mask_2.play()
