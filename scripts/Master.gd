c177ae284cf1e2f8fca95287b835af42eafef3b6
SsbThatsMe <samuel.broms@gmail.com>
2026-01-31 12:13:18 +0100

    fix all the thinggit status!

--- scripts/Master.gd
+++ scripts/Master.gd
@@ -7,36 +7,23 @@ var MASK_TEXTURES : Array[Texture2D] = [
 	preload("res://assets/Placeholder oni mask Red.png")
 ]
 
-var current_masks : Array[int] = []
-var secret_masks : Array = []
-var guess : Array[int] = []
+var current_masks : Array[int]
+var secret_masks : Array[int]
 var masks_in_guess: int = 4;
 
 var log : Array[Array] = []
 
 func _ready() -> void:
-	current_masks = [1,2,3,4]
-	secret_masks = [1,2,3,4]
-	add_guess(2)
-	add_guess(2)
-	add_guess(3)
-	add_guess(4)
-	print(try_solve())
-	add_guess(1)
-	add_guess(2)
-	add_guess(3)
-	add_guess(4)
-	print(try_solve())
-	print(log)
+	current_masks = [0,1,2,3]
+	secret_masks = [0,1,2,3]
 
-func try_solve() -> Vector2:
+func try_solve(guess : Array[int]) -> Vector2:
+	print(guess)
 	var temp = secret_masks.duplicate()
 	var result : Vector2 = Vector2.ZERO
 	for i : int in range(temp.size()):
-		print("temp: ", temp)
-		print("guess: ", guess)
 		if temp[i] == guess[i]:
-			temp[i] = 0
+			temp[i] = -1
 			result.x += 1
 	
 	for j : int in range(temp.size()):
@@ -44,13 +31,4 @@ func try_solve() -> Vector2:
 			result.y += 1
 	
 	log.append([guess.duplicate(), result])
-	guess = []
 	return result
-	
-func add_guess(new_guess : int):
-	print("next step")
-	if guess.size() < secret_masks.size():
-		guess.push_back(new_guess)
-		
-func remove_guess():
-	guess.pop_back()
--- scripts/mask_container.gd
+++ scripts/mask_container.gd
@@ -1,16 +1,23 @@
 extends HBoxContainer
 
+@export
+var input_masks = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	for n in self.get_children():
 		remove_child(n)
 	for i in Master.masks_in_guess:
-		add_child(preload("uid://b3xqkba08wweq").instantiate())
-		print("made mask")
+		var c = preload("uid://b3xqkba08wweq").instantiate()
+		add_child(c)
+		input_masks.append(c)
 	pass # Replace with function body.
 	
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _input(event: InputEvent) -> void:
+	if event is InputEventKey and event.is_pressed():
+		if event.keycode == KEY_ENTER:
+			var guess : Array[int]
+			for m in input_masks:
+				guess.append(m.index)
+			print(Master.try_solve(guess))
