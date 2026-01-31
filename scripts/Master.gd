4b11cc16e054e6f711454219722218e7fa4c538d
LimeVonHanse <emil.hansen98@gmail.com>
2026-01-31 10:47:35 +0100

    Added log and moved master to scripts folder

--- scripts/Master.gd
+++ scripts/Master.gd
@@ -4,6 +4,8 @@ var current_masks : Array[int] = []
 var secret_masks : Array[int] = []
 var guess : Array[int] = []
 
+var log : Array[Array] = []
+
 func _ready() -> void:
 	secret_masks = [1,2,3,4]
 	add_guess(2)
@@ -16,32 +18,23 @@ func _ready() -> void:
 	add_guess(3)
 	add_guess(4)
 	print(try_solve())
-	add_guess(3)
-	add_guess(3)
-	add_guess(3)
-	add_guess(3)
-	print(try_solve())
-	add_guess(2)
-	add_guess(2)
-	add_guess(3)
-	add_guess(4)
-	add_guess(6)
-	print(try_solve())
+	print(log)
 
 func try_solve() -> Vector2:
 	var temp = secret_masks.duplicate()
-	var solution : Vector2 = Vector2.ZERO
+	var result : Vector2 = Vector2.ZERO
 	for i : int in range(temp.size()):
 		if temp[i] == guess[i]:
 			temp[i] = 0
-			solution.x += 1
+			result.x += 1
 	
 	for j : int in range(temp.size()):
 		if temp.has(guess[j]):
-			solution.y += 1
+			result.y += 1
 	
+	log.append([guess.duplicate(), result])
 	guess = []
-	return solution
+	return result
 	
 func add_guess(new_guess : int):
 	print("next step")
