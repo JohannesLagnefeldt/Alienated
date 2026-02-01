7a6cd041416e31f1531583ae7b518745c5416f05
JohannesLagnefeldt <johannes.lagnefeldt@gmail.com>
2026-02-01 16:17:30 +0100

    Music now

--- scripts/music_player.gd
+++ scripts/music_player.gd
@@ -59,8 +59,8 @@ func _process(delta: float) -> void:
 			party_bass.volume_linear = lerp(party_bass.volume_linear, clamp(party_level - 1, 0, 1), delta * tarnsition_strength)
 			party_arp.volume_linear = lerp(party_arp.volume_linear, clamp(party_level - 2, 0, 1), delta * tarnsition_strength)
 		3:
-			alienated_mainmenu.volume_linear = lerp(alienated_mainmenu.volume_linear, 0, delta * tarnsition_strength)
+			alienated_mainmenu.volume_linear = lerp(alienated_mainmenu.volume_linear, 0.0, delta * tarnsition_strength)
 			for i in party.get_children():
-				i.volume_linear = lerp(i.volume_linear, 0, delta * tarnsition_strength)
+				i.volume_linear = lerp(i.volume_linear, 0.0, delta * tarnsition_strength)
 			for i in interact.get_children():
-				i.volume_linear = lerp(i.volume_linear, 1, delta * tarnsition_strength)
+				i.volume_linear = lerp(i.volume_linear, 1.0, delta * tarnsition_strength)
