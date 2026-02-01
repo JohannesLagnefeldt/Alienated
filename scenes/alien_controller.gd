69687ceb1106c0b70a4b64c4a82a05729de1b75e
SsbThatsMe <samuel.broms@gmail.com>
2026-02-01 14:40:51 +0100

    Made Aliens go in and out

---
+++ scenes/alien_controller.gd
@@ -0,0 +1,31 @@
+extends Node2D
+
+@onready var timer: Timer = $Timer
+@onready var character_changer: AnimationPlayer = $CharacterChanger
+
+var state : int = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	character_changer.play("MercuryEnter")
+	Signals.connect("alien_change", change_alien)
+
+func change_alien():
+	state += 1;
+	match state:
+		1:
+			character_changer.play_backwards("MercuryEnter")
+			timer.start()
+		2:
+			character_changer.play_backwards("EyeEnter")
+			timer.start()
+
+		
+				
+
+func _on_timer_timeout() -> void:
+	match state:
+			1:
+				character_changer.play("EyeEnter")
+			2:
+				character_changer.play("GravityEnter")
