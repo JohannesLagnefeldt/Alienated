71e95add6d7c8b3d5717361284043e423b5194f1
SsbThatsMe <samuel.broms@gmail.com>
2026-02-01 19:16:24 +0100

    fix

--- scenes/ui/log_mask.gd
+++ scenes/ui/log_mask.gd
@@ -1,8 +1,8 @@
 extends TextureRect
 
-var react_correct = preload("res://assets/ui/reaction_correct.png")
-var react_exists = preload("res://assets/ui/reaction_exists.png")
-var react_none = preload("res://assets/ui/reaction_none.png")
+var react_correct = preload("res://assets/ui/Green Button.png")
+var react_exists = preload("res://assets/ui/Yellow Light.png")
+var react_none = preload("res://assets/ui/Red Light.png")
 
 @export var react_texture_rect : TextureRect
 @export var guess_texture_rect : TextureRect
