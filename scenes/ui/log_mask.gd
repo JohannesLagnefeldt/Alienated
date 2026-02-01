ad471521926c10e0acb3d46e5141d74620d94606
SsbThatsMe <samuel.broms@gmail.com>
2026-02-01 18:53:30 +0100

    show the guessed mask in the log

--- scenes/ui/log_mask.gd
+++ scenes/ui/log_mask.gd
@@ -5,6 +5,7 @@ var react_exists = preload("res://assets/ui/reaction_exists.png")
 var react_none = preload("res://assets/ui/reaction_none.png")
 
 @export var react_texture_rect : TextureRect
+@export var guess_texture_rect : TextureRect
 
 func set_react(react : PuzzleManager.PUZZLE_RESULT):
 	if react == PuzzleManager.PUZZLE_RESULT.NONE:
@@ -13,3 +14,6 @@ func set_react(react : PuzzleManager.PUZZLE_RESULT):
 		react_texture_rect.texture = react_exists
 	elif react == PuzzleManager.PUZZLE_RESULT.MATCH:
 		react_texture_rect.texture = react_correct
+
+func set_guess(guess : Texture2D):
+	guess_texture_rect.texture = guess
